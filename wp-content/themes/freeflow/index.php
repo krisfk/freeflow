1d6e20a769c90934607814de9eadc68cfb8ef258
kaiyu li <krisfk@gmail.com>
2022-12-29 03:47:50 +0800

    .

--- wp-content/themes/freeflow/index.php
+++ wp-content/themes/freeflow/index.php
@@ -66,7 +66,7 @@ get_header(); ?>
         <div class="col-5">
 
 
-            <table>
+            <table class="float-start">
                 <tr>
                     <td> <img class="home-entry-img"
                             src="http://64.227.13.14/freeflow/wp-content/uploads/2022/12/home-entry-1-photo.jpg" alt="">
@@ -120,7 +120,7 @@ get_header(); ?>
         <div class="col-5">
 
 
-            <table>
+            <table class="float-end">
                 <tr>
 
                     <td class="subtitle-td align-bottom">
