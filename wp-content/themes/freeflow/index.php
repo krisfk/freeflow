557d8af5244f9040c8d429c6cd04656330b4a238
kaiyu li <krisfk@gmail.com>
2022-12-31 04:53:17 +0800

    .

--- wp-content/themes/freeflow/index.php
+++ wp-content/themes/freeflow/index.php
@@ -282,7 +282,6 @@ get_header(); ?>
                             <td>主辦機構</td>
                         </tr>
                         <tr>
-                            <td>
                             <td><a href="javascript:void(0);"><img class="top-icon-img"
                                         src="http://64.227.13.14/freeflow/wp-content/uploads/2022/12/top-icon-1.png"
                                         alt=""></a><a href="javascript:void(0);"><img class="top-icon-img"
@@ -290,7 +289,6 @@ get_header(); ?>
                                         alt=""></a><a href="javascript:void(0);"><img class="top-icon-img"
                                         src="http://64.227.13.14/freeflow/wp-content/uploads/2022/12/top-icon-3.png"
                                         alt=""></a>
-                            </td>
                             </td>
                         </tr>
                     </table>
