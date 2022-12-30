eca4a9c616b92b0011c15b08f265690de776dd2b
kaiyu li <krisfk@gmail.com>
2022-12-31 06:58:08 +0800

    .

--- wp-content/themes/freeflow/page-about.php
+++ wp-content/themes/freeflow/page-about.php
@@ -77,7 +77,7 @@ get_header(); ?>
 
     <div class="row">
 
-        <div class="col-6">
+        <div class="col-7">
 
 
             <div>
@@ -104,7 +104,7 @@ get_header(); ?>
 
         </div>
 
-        <div class="col-6">
+        <div class="col-5">
 
 
             <table class="float-end">
