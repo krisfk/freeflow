092c08f9315731613d67aabfc00c0e1eab3fbfc4
kaiyu li <krisfk@gmail.com>
2022-12-31 05:09:04 +0800

    .

--- wp-content/themes/freeflow/index.php
+++ wp-content/themes/freeflow/index.php
@@ -330,6 +330,15 @@ get_header(); ?>
             </div>
         </div>
 
+        <div class="row footer-small-word">
+            <div class="col-12">
+
+                於新冠肺炎疫情期間拍攝之影像，在場人士均有嚴格遵從當時實行之防疫措施。 <br>
+                所有作品內容均由主辦單位/創作團隊獨立製作，並不代表本計劃及捐助機構之立場或意見。
+
+            </div>
+        </div>
+
     </div>
 
 </div>
