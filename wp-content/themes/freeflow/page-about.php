be362ae0b06c7a6a5ffdc99a9d7be33a6e81448a
kaiyu li <krisfk@gmail.com>
2022-12-31 18:33:56 +0800

    .

--- wp-content/themes/freeflow/page-about.php
+++ wp-content/themes/freeflow/page-about.php
@@ -197,7 +197,7 @@ get_header(); ?>
                 <table class="mb-4">
                     <tr>
                         <td class="green big pe-3">心理 - </td>
-                        <td class="orange">從心出發</td>
+                        <td class="orange middle-size">從心出發</td>
                         </td>
                     </tr>
                 </table>
@@ -207,7 +207,7 @@ get_header(); ?>
                 <table class="mb-4">
                     <tr>
                         <td class="green big pe-3">生理 - </td>
-                        <td class="orange">從身出發</td>
+                        <td class="orange  middle-size">從身出發</td>
 
                     </tr>
                 </table>
