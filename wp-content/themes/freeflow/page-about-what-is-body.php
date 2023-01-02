ad23fa6d9448a0d5b2aa0a5366a60aab92d30183
kaiyu li <krisfk@gmail.com>
2023-01-03 01:21:00 +0900

    .

--- wp-content/themes/freeflow/page-about-what-is-body.php
+++ wp-content/themes/freeflow/page-about-what-is-body.php
@@ -68,7 +68,7 @@ get_header(); ?>
             </div>
 
 
-            <a href="javascript:void(0);" class="home-entry-link mt-4">更多課程資訊</a>
+            <a href="javascript:void(0);" class="home-entry-link mt-4">更多關於雲門舞集舞蹈教室的查詢</a>
 
         </div>
 
