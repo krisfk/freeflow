90dc4567b14d751647905757ae75d7b9bf421d4a
kaiyu li <krisfk@gmail.com>
2023-01-07 03:10:23 +0900

    .

--- wp-content/themes/freeflow/page-news-2.php
+++ wp-content/themes/freeflow/page-news-2.php
@@ -89,8 +89,7 @@ get_header(); ?>
         </div>
         <div class="col-5 text-end">
 
-            <img class="d-inline-block" style="max-height:460px;"
-                src="http://64.227.13.14/freeflow/wp-content/uploads/2023/01/Asset-16@2x.png" alt="">
+
         </div>
 
     </div>
