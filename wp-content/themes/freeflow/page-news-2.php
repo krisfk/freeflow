00ba44114cae9f6247f8eea3593d4164656b7889
kaiyu li <krisfk@gmail.com>
2023-01-07 03:04:52 +0900

    .

--- wp-content/themes/freeflow/page-news-2.php
+++ wp-content/themes/freeflow/page-news-2.php
@@ -69,6 +69,35 @@ get_header(); ?>
 </div>
 
 
+<div class="container yellow-bg-div gx-0 mt-60px">
+
+
+    <h2 class="purple">賽馬會「自主・流」身體律動計劃 <br>
+        年度展覽《我動故我在》</h2>
+
+
+    <div class="row mt-4">
+
+        <div class="col-7">
+
+            在《我動故我在》，我們將分享青少年在這一年身體律動課的體驗及向公眾推廣身體律動及對自我身體的關注。在這空間與大眾討論女性標準，探討這些期望為本地青少女帶來的影響，協助公眾認識身體律動，關顧自己的身心靈需要，加深對性別議題的認識。
+            <br> <br>
+            是次活動結合律動體驗課和藝術展覽，展出了本計劃的一年總結。展覽主要分為三部分: 故我在、我自在、漫動作。
+
+        </div>
+        <div class="col-5 text-end">
+
+            <img class="d-inline-block" style="max-height:460px;"
+                src="http://64.227.13.14/freeflow/wp-content/uploads/2023/01/Asset-16@2x.png" alt="">
+        </div>
+
+    </div>
+
+
+</div>
+
+
+
 
 
 
