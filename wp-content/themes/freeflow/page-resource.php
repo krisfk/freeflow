774d277cd9cba98aa857bb8d4c8f55d19189100e
kaiyu li <krisfk@gmail.com>
2023-01-08 03:38:13 +0900

    .

--- wp-content/themes/freeflow/page-resource.php
+++ wp-content/themes/freeflow/page-resource.php
@@ -26,7 +26,7 @@ get_header(); ?>
 
 
 
-<div class="banner-div" style="background:url(http://64.227.13.14/freeflow/wp-content/uploads/2023/01/news-banner-2-scaled.jpg);background-size: cover;
+<div class="banner-div" style="background:url(http://64.227.13.14/freeflow/wp-content/uploads/2023/01/resource-banner-scaled.jpg);background-size: cover;
     background-position: center center;
     display: block;
     width: 100%;
