0512421a715a7ac75f27ff1d77f5de460f10428c
kaiyu li <krisfk@gmail.com>
2023-01-08 04:24:26 +0900

    .

--- wp-content/themes/freeflow/page-resource.php
+++ wp-content/themes/freeflow/page-resource.php
@@ -104,8 +104,6 @@ get_header(); ?>
 
 
 <div class="container yellow-bg-div gx-0 mt-60px">
-
-
     <div class="row">
         <div class="col-6">
 
@@ -198,8 +196,199 @@ get_header(); ?>
 
         </div>
     </div>
+</div>
 
 
+<div class="container yellow-bg-div gx-0 mt-60px">
+    <div class="row">
+        <div class="col-6">
+
+            <img class="w-100" src="http://64.227.13.14/freeflow/wp-content/uploads/2023/01/blog-temp-img-1.png" alt="">
+
+
+
+            <h2 class="purple big mt-4">標題文章和故事</h2>
+
+            <div class="orange">4 Nov 2022</div>
+
+            <div class="mt-3">有關賽馬會「自主‧流」身體律動計劃的文章及資料會於這裡與大家分享。內容包括由青少女及計劃社工撰寫的文章和故事。以及，與香港浸會大學合作的研究正在進行，結果詳情將容後公佈
+                <a href="javascript:void(0);" class="orange more-link">... more</a>
+            </div>
+
+            <div class="mt-3"> <a href="javascript:void(0);" class="blog-tag me-3">#性</a>
+                <a href="javascript:void(0);" class="blog-tag me-3">#身體</a>
+                <a href="javascript:void(0);" class="blog-tag me-3">#自我</a>
+                <a href="javascript:void(0);" class="blog-tag">#感想</a>
+            </div>
+
+
+            <div class="blog-line mt-4"></div>
+
+
+
+            <div class="row mt-4">
+
+
+                <div class="col-3">214 views</div>
+                <div class="col-9 text-end">
+
+                    <span>10</span>
+
+                    <a href="javascript:void(0);" class="d-inline-block">
+                        <img class="heart-icon" src="http://64.227.13.14/freeflow/wp-content/uploads/2023/01/heart.png"
+                            alt=""></a>
+
+                    <a href="javascript:void(0);" class="d-inline-block three-dots-a ms-4"> <img class="three-dots"
+                            src="http://64.227.13.14/freeflow/wp-content/uploads/2023/01/3dots.png" alt=""></a>
+                </div>
+
+            </div>
+
+
+        </div>
+        <div class="col-6">
+
+            <img class="w-100" src="http://64.227.13.14/freeflow/wp-content/uploads/2023/01/blog-temp-img-2.png" alt="">
+
+
+
+            <h2 class="purple big mt-4">標題文章和故事</h2>
+
+            <div class="orange">4 Nov 2022</div>
+
+            <div class="mt-3">有關賽馬會「自主‧流」身體律動計劃的文章及資料會於這裡與大家分享。內容包括由青少女及計劃社工撰寫的文章和故事。以及，與香港浸會大學合作的研究正在進行，結果詳情將容後公佈
+                <a href="javascript:void(0);" class="orange more-link">... more</a>
+            </div>
+
+            <div class="mt-3"> <a href="javascript:void(0);" class="blog-tag me-3">#性</a>
+                <a href="javascript:void(0);" class="blog-tag me-3">#身體</a>
+                <a href="javascript:void(0);" class="blog-tag me-3">#自我</a>
+                <a href="javascript:void(0);" class="blog-tag">#感想</a>
+            </div>
+
+
+            <div class="blog-line mt-4"></div>
+
+
+
+            <div class="row mt-4">
+
+
+                <div class="col-3">214 views</div>
+                <div class="col-9 text-end">
+
+                    <span>10</span>
+
+                    <a href="javascript:void(0);" class="d-inline-block">
+                        <img class="heart-icon" src="http://64.227.13.14/freeflow/wp-content/uploads/2023/01/heart.png"
+                            alt=""></a>
+
+                    <a href="javascript:void(0);" class="d-inline-block three-dots-a ms-4"> <img class="three-dots"
+                            src="http://64.227.13.14/freeflow/wp-content/uploads/2023/01/3dots.png" alt=""></a>
+                </div>
+
+            </div>
+
+
+        </div>
+    </div>
+</div>
+
+
+
+<div class="container yellow-bg-div gx-0 mt-60px">
+    <div class="row">
+        <div class="col-6">
+
+            <img class="w-100" src="http://64.227.13.14/freeflow/wp-content/uploads/2023/01/blog-temp-img-1.png" alt="">
+
+
+
+            <h2 class="purple big mt-4">標題文章和故事</h2>
+
+            <div class="orange">4 Nov 2022</div>
+
+            <div class="mt-3">有關賽馬會「自主‧流」身體律動計劃的文章及資料會於這裡與大家分享。內容包括由青少女及計劃社工撰寫的文章和故事。以及，與香港浸會大學合作的研究正在進行，結果詳情將容後公佈
+                <a href="javascript:void(0);" class="orange more-link">... more</a>
+            </div>
+
+            <div class="mt-3"> <a href="javascript:void(0);" class="blog-tag me-3">#性</a>
+                <a href="javascript:void(0);" class="blog-tag me-3">#身體</a>
+                <a href="javascript:void(0);" class="blog-tag me-3">#自我</a>
+                <a href="javascript:void(0);" class="blog-tag">#感想</a>
+            </div>
+
+
+            <div class="blog-line mt-4"></div>
+
+
+
+            <div class="row mt-4">
+
+
+                <div class="col-3">214 views</div>
+                <div class="col-9 text-end">
+
+                    <span>10</span>
+
+                    <a href="javascript:void(0);" class="d-inline-block">
+                        <img class="heart-icon" src="http://64.227.13.14/freeflow/wp-content/uploads/2023/01/heart.png"
+                            alt=""></a>
+
+                    <a href="javascript:void(0);" class="d-inline-block three-dots-a ms-4"> <img class="three-dots"
+                            src="http://64.227.13.14/freeflow/wp-content/uploads/2023/01/3dots.png" alt=""></a>
+                </div>
+
+            </div>
+
+
+        </div>
+        <div class="col-6">
+
+            <img class="w-100" src="http://64.227.13.14/freeflow/wp-content/uploads/2023/01/blog-temp-img-2.png" alt="">
+
+
+
+            <h2 class="purple big mt-4">標題文章和故事</h2>
+
+            <div class="orange">4 Nov 2022</div>
+
+            <div class="mt-3">有關賽馬會「自主‧流」身體律動計劃的文章及資料會於這裡與大家分享。內容包括由青少女及計劃社工撰寫的文章和故事。以及，與香港浸會大學合作的研究正在進行，結果詳情將容後公佈
+                <a href="javascript:void(0);" class="orange more-link">... more</a>
+            </div>
+
+            <div class="mt-3"> <a href="javascript:void(0);" class="blog-tag me-3">#性</a>
+                <a href="javascript:void(0);" class="blog-tag me-3">#身體</a>
+                <a href="javascript:void(0);" class="blog-tag me-3">#自我</a>
+                <a href="javascript:void(0);" class="blog-tag">#感想</a>
+            </div>
+
+
+            <div class="blog-line mt-4"></div>
+
+
+
+            <div class="row mt-4">
+
+
+                <div class="col-3">214 views</div>
+                <div class="col-9 text-end">
+
+                    <span>10</span>
+
+                    <a href="javascript:void(0);" class="d-inline-block">
+                        <img class="heart-icon" src="http://64.227.13.14/freeflow/wp-content/uploads/2023/01/heart.png"
+                            alt=""></a>
+
+                    <a href="javascript:void(0);" class="d-inline-block three-dots-a ms-4"> <img class="three-dots"
+                            src="http://64.227.13.14/freeflow/wp-content/uploads/2023/01/3dots.png" alt=""></a>
+                </div>
+
+            </div>
+
+
+        </div>
+    </div>
 </div>
 
 
