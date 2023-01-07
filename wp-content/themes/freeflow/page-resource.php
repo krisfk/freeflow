1807c580d7a368bb1cdbbf2120a62c7ae6a03b40
kaiyu li <krisfk@gmail.com>
2023-01-08 03:50:23 +0900

    .

--- wp-content/themes/freeflow/page-resource.php
+++ wp-content/themes/freeflow/page-resource.php
@@ -53,7 +53,7 @@ get_header(); ?>
 
 
 
-            <article class="vertical-article float-left me-4">
+            <article class="vertical-article float-start me-4">
                 <h2 class="orange">律動資料庫
                 </h2>
             </article>
