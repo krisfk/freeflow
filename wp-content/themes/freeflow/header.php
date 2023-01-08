70957a3d77b99d4768ca76495387659c83777f5b
kaiyu li <krisfk@gmail.com>
2023-01-09 03:40:28 +0900

    .

--- wp-content/themes/freeflow/header.php
+++ wp-content/themes/freeflow/header.php
@@ -48,7 +48,7 @@
 
 
         <div class="menu-btn-icon"></div>
-        <img src="http://64.227.13.14/freeflow/wp-content/uploads/2022/12/menu-btn.png" alt="">
+        <!-- <img src="http://64.227.13.14/freeflow/wp-content/uploads/2022/12/menu-btn.png" alt=""> -->
         <div class="menu-btn-txt">Menu</div>
     </a>
 
