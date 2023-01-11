8ebb4de8b57e296b68fd997fd1b8519b72ecc31d
kaiyu li <krisfk@gmail.com>
2023-01-12 06:37:52 +0800

    .

--- wp-content/themes/freeflow/header.php
+++ wp-content/themes/freeflow/header.php
@@ -47,7 +47,66 @@
                 <li><a href="javascript:void(0);">EN</a></li>
             </ul>
 
-            <ul class="first-level">
+
+
+            <?php
+                                    $main_menu = wp_get_menu_array('main menu');
+foreach ($main_menu as $menu_item) {
+
+$url = $menu_item['url'];
+$title = $menu_item['title'];
+$class = $menu_item['class'];
+
+$temp_arr=explode(get_site_url(),$url);
+$slug=str_replace('/en/','',$temp_arr[1]);
+$slug=str_replace('/cn/','',$slug);
+$slug=str_replace('/','',$slug);
+
+
+if(count($menu_item['children']))
+{
+  
+    echo '<li><a class="level-1 parent '.$class.'" href="'.$url.'">'.$title;
+    ?>
+            <img class="arrow" src="<?php echo get_template_directory_uri();?>/assets/images/white-arrow-enter.png"
+                alt="">
+
+            <?php
+    echo'</a>';
+
+ 
+    echo '<ul class="mobile-menu-submenu">';
+?>
+
+            <?php
+    
+    foreach ($menu_item['children'] as $sub_menu_item) 
+    {
+        $sub_url = $sub_menu_item['url'];
+        $sub_title = $sub_menu_item['title'];
+        
+        $sub_temp_arr=explode(get_site_url(),$sub_url);
+        $sub_slug=str_replace('/en/','',$sub_temp_arr[1]);
+        $sub_slug=str_replace('/cn/','',$sub_slug);
+        $sub_slug=str_replace('/','',$sub_slug);
+        echo'<li><a class="'.$sub_slug.'" href="'.$sub_url.'">'.$sub_title.'</a></li>';
+    }
+    echo '</ul>';
+
+}
+else
+{
+echo '<li><a class="level-1 '.$slug.' '.$class.'" href="'.$url.'">'.$title.'</a>';
+
+}
+echo'</li>';
+
+
+}
+
+?>
+
+            <!-- <ul class="first-level">
                 <li><a href="javascript:void(0);">關於我們</a>
 
                     <a href="javascript:void(0);" class="menu-arrow"></a>
@@ -74,7 +133,7 @@
                 <li><a href="javascript:void(0);">與我們合作</a></li>
             </ul>
 
-
+ -->
 
 
             <ul class="sns-menu-ul">
