e4efa4e9d4bf11fef54adda1ab5d60b895d62413
kaiyu li <krisfk@gmail.com>
2023-01-12 06:35:20 +0800

    .

--- wp-content/themes/freeflow/functions.php
+++ wp-content/themes/freeflow/functions.php
@@ -678,4 +678,46 @@ function add_slug_body_class( $classes ) {
 	add_action('init', 'app_output_buffer');
 
 
-	
+
+function wp_get_menu_array($current_menu='Main Menu') {
+
+	$menu_array = wp_get_nav_menu_items($current_menu);
+
+	$menu = array();
+
+	function populate_children($menu_array, $menu_item)
+	{
+		$children = array();
+		if (!empty($menu_array)){
+			foreach ($menu_array as $k=>$m) {
+				if ($m->menu_item_parent == $menu_item->ID) {
+					$children[$m->ID] = array();
+					$children[$m->ID]['ID'] = $m->ID;
+					$children[$m->ID]['title'] = $m->title;
+					$children[$m->ID]['url'] = $m->url;
+					// $children[$m->ID]['class'] = 'fdsaf';
+
+					unset($menu_array[$k]);
+					$children[$m->ID]['children'] = populate_children($menu_array, $m);
+				}
+			}
+		};
+
+		return $children;
+	}
+
+	foreach ($menu_array as $m) {
+		if (empty($m->menu_item_parent)) {
+			$menu[$m->ID] = array();
+			$menu[$m->ID]['ID'] = $m->ID;
+			$menu[$m->ID]['title'] = $m->title;
+			$menu[$m->ID]['url'] = $m->url;
+			$menu[$m->ID]['class'] = $m->classes[0];
+
+			$menu[$m->ID]['children'] = populate_children($menu_array, $m);
+		}
+	}
+
+	return $menu;
+
+}
