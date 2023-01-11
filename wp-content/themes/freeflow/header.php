9d625f89e5f96237e4032b86e2039bf1ea69e309
kaiyu li <krisfk@gmail.com>
2023-01-12 06:44:44 +0800

    .

--- wp-content/themes/freeflow/header.php
+++ wp-content/themes/freeflow/header.php
@@ -49,7 +49,7 @@
 
 
 
-            <ul class="first-level">
+            <ul class="level-1">
                 <?php
                                     $main_menu = wp_get_menu_array('main menu');
 foreach ($main_menu as $menu_item) {
@@ -76,7 +76,7 @@ if(count($menu_item['children']))
     echo'</a>';
 
  
-    echo '<ul class="mobile-menu-submenu">';
+    echo '<ul class="mobile-menu-submenu level-2">';
 ?>
 
                 <?php
