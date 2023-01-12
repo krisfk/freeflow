41b5f700c2dd35d0f1b53d6a7448824c2acdad38
kaiyu li <krisfk@gmail.com>
2023-01-13 00:11:26 +0800

    .

--- wp-content/themes/freeflow/index.php
+++ wp-content/themes/freeflow/index.php
@@ -174,7 +174,7 @@ get_header(); ?>
     alt=""> -->
 
 
-<iframe width="560" height="315" src="https://www.youtube.com/embed/9tKGHIdpEac" title="YouTube video player"
+<iframe width="560" height="315" src="https://www.youtube.com/embed/9tKGHIdpEac?rel=0" title="YouTube video player"
     frameborder="0"
     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
     allowfullscreen></iframe>
