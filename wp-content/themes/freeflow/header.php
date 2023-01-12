d5cfefebd1b9c9662596eb1e544eeb5b6d906cf9
kaiyu li <krisfk@gmail.com>
2023-01-13 02:19:37 +0800

    .

--- wp-content/themes/freeflow/header.php
+++ wp-content/themes/freeflow/header.php
@@ -349,7 +349,7 @@ echo'</li>';
         <!-- <span id="circle" class="circle"> -->
 
 
-        <svg id="circle" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+        <svg id="circle" class="circle" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
             <circle cx="50" cy="50" r="50" />
         </svg>
 
