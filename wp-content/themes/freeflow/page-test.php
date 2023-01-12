976ac3583cc6d6bafe4cb3c5baa4b448dfdd2fc4
kaiyu li <krisfk@gmail.com>
2023-01-12 18:18:42 +0800

    .

--- wp-content/themes/freeflow/page-test.php
+++ wp-content/themes/freeflow/page-test.php
@@ -11,6 +11,61 @@
 
 get_header();
 ?>
-test<?php
+
+<!--
+As mentioned in the official Bootstrap 3 documentation (http://getbootstrap.com/components/#thumbnails): You can either do it like in this pen, or see more options at:
+
+https://masonry.desandro.com/
+https://isotope.metafizzy.co/
+http://salvattore.com/
+-->
+
+<div class="container">
+    <div class="row masonry-grid">
+        <div class="col-md-6 col-lg-4 masonry-column">
+            <div>
+                <a href="http://placeholder.com" class="thumbnail"><img
+                        src="http://via.placeholder.com/400x450/3F51B5/fff"></a>
+            </div>
+            <div>
+                <a href="http://placeholder.com" class="thumbnail"><img
+                        src="http://via.placeholder.com/400x250/3F51B5/fff"></a>
+            </div>
+            <div>
+                <a href="http://placeholder.com" class="thumbnail"><img
+                        src="http://via.placeholder.com/400x550/3F51B5/fff"></a>
+            </div>
+        </div>
+        <div class="col-md-6 col-lg-4 masonry-column">
+            <div>
+                <a href="http://placeholder.com" class="thumbnail"><img
+                        src="http://via.placeholder.com/400x150/3F51B5/fff"></a>
+            </div>
+            <div>
+                <a href="http://placeholder.com" class="thumbnail"><img
+                        src="http://via.placeholder.com/400x250/3F51B5/fff"></a>
+            </div>
+            <div>
+                <a href="http://placeholder.com" class="thumbnail"><img
+                        src="http://via.placeholder.com/400x650/3F51B5/fff"></a>
+            </div>
+        </div>
+        <div class="col-md-6 col-lg-4 masonry-column">
+            <div>
+                <a href="http://placeholder.com" class="thumbnail"><img
+                        src="http://via.placeholder.com/400x550/3F51B5/fff"></a>
+            </div>
+            <div>
+                <a href="http://placeholder.com" class="thumbnail"><img
+                        src="http://via.placeholder.com/400x450/3F51B5/fff"></a>
+            </div>
+            <div>
+                <a href="http://placeholder.com" class="thumbnail"><img
+                        src="http://via.placeholder.com/400x450/3F51B5/fff"></a>
+            </div>
+        </div>
+    </div>
+</div>
+<?php
 
 get_footer();
