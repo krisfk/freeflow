3782aee6760e63565fb8e18366dd107c3262c7a9
kaiyu li <krisfk@gmail.com>
2023-01-12 18:17:50 +0800

    .

--- wp-content/themes/freeflow/page-test.php
+++ wp-content/themes/freeflow/page-test.php
@@ -1,15 +1,16 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
+/**
+ * The template for displaying all single posts
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty_One
+ * @since Twenty Twenty-One 1.0
+ */
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
+get_header();
+?>
+test<?php
 
-<body>
-    test
-</body>
-
-</html>
+get_footer();
