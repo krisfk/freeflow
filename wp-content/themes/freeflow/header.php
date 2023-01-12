13f39d8a6352985cf32d6c5f0e13772e83073b4d
kaiyu li <krisfk@gmail.com>
2023-01-12 22:55:54 +0800

    .

--- wp-content/themes/freeflow/header.php
+++ wp-content/themes/freeflow/header.php
@@ -63,6 +63,11 @@
             <div class=" row">
 
 
+                <div class="col-12">
+
+                    <div class="lightbox-close-btn"></div>
+
+                </div>
                 <div class="col-3">
                     <h2 class="orange">使用條款</h2>
                 </div>
