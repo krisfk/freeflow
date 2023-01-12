5f102fa8bcd5de079650483efe55b94ec59aa8fb
kaiyu li <krisfk@gmail.com>
2023-01-12 17:13:15 +0800

    .

--- wp-content/themes/freeflow/page-collaborate.php
+++ wp-content/themes/freeflow/page-collaborate.php
@@ -30,7 +30,7 @@ get_header(); ?>
 
 
     <a href="<?php echo get_site_url();?>" class="freeflow-logo-a"><img
-            src="http://64.227.13.14/freeflow/wp-content/uploads/2022/12/freeflow-logo.png" alt=""></a>
+            src="http://64.227.13.14/freeflow/wp-content/uploads/2023/01/collaborate-banner-scaled.jpg" alt=""></a>
 
 </div>
 
