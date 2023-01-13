1cb7cb787cfbca64cd3f914f55df8580fade7e9a
kaiyu li <krisfk@gmail.com>
2023-01-13 13:34:35 +0800

    .

--- wp-content/themes/freeflow/page-news-1.php
+++ wp-content/themes/freeflow/page-news-1.php
@@ -52,9 +52,9 @@ get_header(); ?>
     <table class="news-tpl-1-img-table w-100">
         <tr>
             <td>
-                <a href="javascript:void(0);" class="album-a d-block">
+                <a href="javascript:void(0);" class="album-a">
                     <img src="http://64.227.13.14/freeflow/wp-content/uploads/2023/01/Asset-7@2x-1.png" alt=""></a>
-                <a href="javascript:void(0);" class="album-a  d-block"><img
+                <a href="javascript:void(0);" class="album-a "><img
                         src="http://64.227.13.14/freeflow/wp-content/uploads/2023/01/Asset-8@2x-1.png" alt=""></a>
 
             </td>
