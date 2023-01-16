65559f72d3bd166ddaf5e12e675f637898aefbe0
kaiyu li <krisfk@gmail.com>
2023-01-16 13:27:50 +0800

    .

--- wp-content/themes/freeflow/page-about.php
+++ wp-content/themes/freeflow/page-about.php
@@ -261,7 +261,7 @@ get_header(); ?>
 
                         <a href="javascript:void(0);" class="album-a"
                             rel="http://64.227.13.14/freeflow/wp-content/uploads/2023/01/DSC00530.jpg">
-                            <img class="entry-img"
+                            <img class="entry-img  row-img-min-400"
                                 src="http://64.227.13.14/freeflow/wp-content/uploads/2022/12/Asset-2@2x.png" alt="">
                         </a>
                     </td>
