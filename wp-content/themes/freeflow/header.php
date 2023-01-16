e813f408a06f34cd1b072fb72440f07b1b4a7082
kaiyu li <krisfk@gmail.com>
2023-01-16 14:07:15 +0800

    .

--- wp-content/themes/freeflow/header.php
+++ wp-content/themes/freeflow/header.php
@@ -575,7 +575,8 @@ echo'</li>';
 
             $('.album-a').click(function() {
 
-                console.log($(this))
+                console.log($(this).eq());
+                // console.log($(this))
 
                 if ($(this).attr('rel')) {
                     // alert(6);
