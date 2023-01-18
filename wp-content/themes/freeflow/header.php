512b6a36793281e019bd71a24d6f2a736c20ac98
kaiyu li <krisfk@gmail.com>
2023-01-19 05:40:57 +0800

    .

--- wp-content/themes/freeflow/header.php
+++ wp-content/themes/freeflow/header.php
@@ -480,7 +480,7 @@ echo'</li>';
 
                 $('#cursor').fadeIn(1000);
 
-                $('#page').height($('body').height());
+                $('#page').height($('body').height() - $('.banner-img').height());
             }, 1000);
 
 
