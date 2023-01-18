760400598bf8f2f11d792f7896d00daba0f6f311
kaiyu li <krisfk@gmail.com>
2023-01-18 14:50:27 +0800

    .

--- wp-content/themes/freeflow/footer.php
+++ wp-content/themes/freeflow/footer.php
@@ -113,10 +113,10 @@
                 </div>
 
 
-                <div class="row align-items-center">
+                <div class="row align-items-center mt-4">
 
                     <div class="col-6">
-                        <ul class="footer-menu-ul p-0  mt-4">
+                        <ul class="footer-menu-ul p-0  ">
                             <li>&copy; 2022 JC FreeFlow</li>
                             <li><a href="javascript:void(0);" class="term-1-btn">使用條款</a></li>
                             <li><a href="javascript:void(0);" class="term-2-btn">私隱政策</a></li>
