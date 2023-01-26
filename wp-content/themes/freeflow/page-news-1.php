4268439f9feebebfbdd599a7dd85cf2cafd12cc6
kaiyu li <krisfk@gmail.com>
2023-01-27 05:39:00 +0800

    .

--- wp-content/themes/freeflow/page-news-1.php
+++ wp-content/themes/freeflow/page-news-1.php
@@ -71,8 +71,19 @@ get_header(); ?>
 
     <div class="row news-tpl-1-img-table mobile">
 
-        <div class="col-6"></div>
-        <div class="col-6"></div>
+        <div class="col-6">
+
+            <a href="javascript:void(0);" class="album-a mobile-keep"
+                rel="http://64.227.13.14/freeflow/wp-content/uploads/2023/01/vlcsnap-2022-07-21-19h29m05s552.jpg">
+                <img src="http://64.227.13.14/freeflow/wp-content/uploads/2023/01/Asset-7@2x-1.png" alt=""></a>
+
+        </div>
+        <div class="col-6">
+
+            <a href="javascript:void(0);" class="album-a  mobile-keep"
+                rel="http://64.227.13.14/freeflow/wp-content/uploads/2023/01/vlcsnap-2022-07-21-19h29m05s552.jpg">
+                <img src="http://64.227.13.14/freeflow/wp-content/uploads/2023/01/Asset-7@2x-1.png" alt=""></a>
+        </div>
     </div>
 
 
