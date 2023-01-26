33a438ff381aa4d51c05d4896c292ddd70af752c
kaiyu li <krisfk@gmail.com>
2023-01-26 15:29:45 +0800

    .

--- wp-content/themes/freeflow/page-about-curriculum.php
+++ wp-content/themes/freeflow/page-about-curriculum.php
@@ -266,7 +266,7 @@ get_header(); ?>
 
                     <div class="row">
 
-                        <div class="col-5">
+                        <div class="col-5 text-center">
                             <div class="d-inline-block "
                                 style="transform:rotate(90deg);    position: relative;top: 8px;"> <img
                                     class="step-arrow"
