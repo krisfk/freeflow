d4ef79f933f076a28f4371d90f42830ca6d91264
kaiyu li <krisfk@gmail.com>
2023-01-27 06:07:23 +0800

    .

--- wp-content/themes/freeflow/page-news-1.php
+++ wp-content/themes/freeflow/page-news-1.php
@@ -73,14 +73,14 @@ get_header(); ?>
 
         <div class="col-6">
 
-            <a href="javascript:void(0);" class="album-a mobile-keep">
+            <a href="javascript:void(0);" class="m-album-a">
                 <img class="w-100" src="http://64.227.13.14/freeflow/wp-content/uploads/2023/01/Asset-7@2x-1.png"
                     alt=""></a>
 
         </div>
         <div class="col-6">
 
-            <a href="javascript:void(0);" class="album-a  mobile-keep ">
+            <a href="javascript:void(0);" class="m-album-a ">
                 <img class="w-100" src="http://64.227.13.14/freeflow/wp-content/uploads/2023/01/Asset-7@2x-1.png"
                     alt=""></a>
         </div>
@@ -118,14 +118,14 @@ get_header(); ?>
 
         <div class="col-6">
 
-            <a href="javascript:void(0);" class="album-a mobile-keep">
+            <a href="javascript:void(0);" class="m-album-a">
                 <img class="w-100" src="http://64.227.13.14/freeflow/wp-content/uploads/2023/01/Asset-10@2x.png"
                     alt=""></a>
 
         </div>
         <div class="col-6">
 
-            <a href="javascript:void(0);" class="album-a  mobile-keep ">
+            <a href="javascript:void(0);" class="m-album-a ">
                 <img class="w-100" src="http://64.227.13.14/freeflow/wp-content/uploads/2023/01/Asset-10@2x.png"
                     alt=""></a>
         </div>
@@ -163,14 +163,14 @@ get_header(); ?>
 
         <div class="col-6">
 
-            <a href="javascript:void(0);" class="album-a mobile-keep">
+            <a href="javascript:void(0);" class="m-album-a">
                 <img class="w-100" src="http://64.227.13.14/freeflow/wp-content/uploads/2023/01/Asset-11@2x-1.png"
                     alt=""></a>
 
         </div>
         <div class="col-6">
 
-            <a href="javascript:void(0);" class="album-a  mobile-keep ">
+            <a href="javascript:void(0);" class="m-album-a ">
                 <img class="w-100" src="http://64.227.13.14/freeflow/wp-content/uploads/2023/01/Asset-11@2x-1.png"
                     alt=""></a>
         </div>
@@ -206,14 +206,14 @@ get_header(); ?>
 
         <div class="col-6">
 
-            <a href="javascript:void(0);" class="album-a mobile-keep">
+            <a href="javascript:void(0);" class="m-album-a">
                 <img class="w-100" src="http://64.227.13.14/freeflow/wp-content/uploads/2023/01/Asset-14@2x.png"
                     alt=""></a>
 
         </div>
         <div class="col-6">
 
-            <a href="javascript:void(0);" class="album-a  mobile-keep ">
+            <a href="javascript:void(0);" class="m-album-a">
                 <img class="w-100" src="http://64.227.13.14/freeflow/wp-content/uploads/2023/01/Asset-14@2x.png"
                     alt=""></a>
         </div>
