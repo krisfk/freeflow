aed73f5df67303d4c03e95fd0f20662b5a7ee928
kaiyu li <krisfk@gmail.com>
2023-01-26 14:56:44 +0800

    .

--- wp-content/themes/freeflow/page-about-curriculum.php
+++ wp-content/themes/freeflow/page-about-curriculum.php
@@ -202,7 +202,7 @@ get_header(); ?>
                         <td></td>
                         <td></td>
                         <td></td>
-                        <td class="pt-2 pe-2 ps-2 text-center">
+                        <td class="pe-2 ps-2 text-center">
                             <div class="d-inline-block " style="transform:rotate(90deg);"> <img class="step-arrow"
                                     src="<?php echo get_template_directory_uri(); ?>/assets/images/orange-arrow.png"
                                     alt=""></div>
