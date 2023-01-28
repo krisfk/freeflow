44cfa698102b010a18bbad8ded7e9d82555856bc
kaiyu li <krisfk@gmail.com>
2023-01-29 07:23:30 +0800

    .

--- wp-content/themes/freeflow/search.php
+++ wp-content/themes/freeflow/search.php
@@ -1,65 +1,613 @@
 <?php
 /**
- * The template for displaying search results pages
+ * The main template file
  *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#search-result
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
  *
  * @package WordPress
  * @subpackage Twenty_Twenty_One
  * @since Twenty Twenty-One 1.0
  */
 
-get_header();
+get_header(); ?>
 
-if ( have_posts() ) {
-	?>
-	<header class="page-header alignwide">
-		<h1 class="page-title">
-			<?php
-			printf(
-				/* translators: %s: Search term. */
-				esc_html__( 'Results for "%s"', 'twentytwentyone' ),
-				'<span class="page-description search-term">' . esc_html( get_search_query() ) . '</span>'
-			);
-			?>
-		</h1>
-	</header><!-- .page-header -->
+<?php if ( is_home() && ! is_front_page() && ! empty( single_post_title( '', false ) ) ) : ?>
+<header class="page-header alignwide">
+    <h1 class="page-title"><?php single_post_title(); ?></h1>
+</header><!-- .page-header -->
+<?php endif; ?>
 
-	<div class="search-result-count default-max-width">
-		<?php
-		printf(
-			esc_html(
-				/* translators: %d: The number of search results. */
-				_n(
-					'We found %d result for your search.',
-					'We found %d results for your search.',
-					(int) $wp_query->found_posts,
-					'twentytwentyone'
-				)
-			),
-			(int) $wp_query->found_posts
-		);
-		?>
-	</div><!-- .search-result-count -->
-	<?php
-	// Start the Loop.
-	while ( have_posts() ) {
-		the_post();
 
-		/*
-		 * Include the Post-Format-specific template for the content.
-		 * If you want to override this in a child theme, then include a file
-		 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-		 */
-		get_template_part( 'template-parts/content/content-excerpt', get_post_format() );
-	} // End the loop.
 
-	// Previous/next page navigation.
-	twenty_twenty_one_the_posts_navigation();
 
-	// If no content, include the "No posts found" template.
-} else {
-	get_template_part( 'template-parts/content/content-none' );
+
+<a href="<?php echo get_site_url();?>" class="freeflow-logo-a"><img
+        src="http://64.227.13.14/freeflow/wp-content/uploads/2022/12/freeflow-logo.png" alt=""></a>
+
+<div class="banner-div" style="">
+
+    <img class="banner-img" src="http://64.227.13.14/freeflow/wp-content/uploads/2023/01/resource-banner-scaled.jpg"
+        alt="">
+
+    <img class="banner-img mobile" src="http://64.227.13.14/freeflow/wp-content/uploads/2023/01/Asset-1@2x-50-4.jpg"
+        alt="">
+
+
+
+
+
+
+</div>
+
+<div class="container  gx-0 mt-120px  yellow-bg-div ">
+
+
+
+    <div class="row mt-4 mobile-reverse-row">
+
+        <div class="col-lg-6 col-md-12 col-sm-12 col-12  position-relative  mt-0">
+
+
+
+
+
+            <article class="vertical-article float-start me-4">
+                <h2 class="orange">律動資料庫
+                </h2>
+            </article>
+
+            <div>
+
+                有關賽馬會「自主．流」身體律動計劃的文章及資料會於這裡與大家分享。內容包括由青少女及計劃社工撰寫的文章和故事。 <br><br>
+
+                以及，與香港浸會大學合作的研究正在進行，結果詳情將容後公佈。
+
+            </div>
+
+
+
+
+
+
+
+        </div>
+        <div class="col-lg-6 col-md-12 col-sm-12 col-12  position-relative  mt-0">
+            <a href="javascript:void(0);" class="album-a mobile-keep"
+                rel="http://64.227.13.14/freeflow/wp-content/uploads/2023/01/Asset-1@2x-50.jpg">
+                <img class="w-100" src="http://64.227.13.14/freeflow/wp-content/uploads/2023/01/Asset-2@2x-1.png"
+                    alt="">
+            </a>
+        </div>
+
+    </div>
+
+
+</div>
+
+
+
+
+<div class="container yellow-bg-div gx-0 mt-120px">
+
+
+    <div class="row">
+
+        <div class="col-12 position-relative">
+
+
+            <div class="d-inline-block w-90">
+                <a href="<?php echo get_site_url();?>/resource" class="blog-tag me-1">All Articles</a>
+                <?php
+
+$tags = get_tags(array(
+'hide_empty' => false
+));
+foreach ($tags as $tag) {
+?>
+
+                <a href="<?php echo get_site_url();?>/tag/<?php echo $tag->slug; ?>"
+                    class="blog-tag me-1">#<?php echo  $tag->name;?></a>
+                <?php
+
+}
+?>
+            </div>
+
+
+            <a href="javascript:void(0);" class="search-icon-a"><img
+                    src="<?php echo get_template_directory_uri(); ?>/assets/images/search-icon.png" alt=""></a>
+
+
+            <div class="search-ui-div">
+                <?php
+
+        
+        ?>
+                <?php echo do_shortcode('[wpdreams_ajaxsearchlite]'); ?>
+            </div>
+
+        </div>
+    </div>
+
+
+
+
+
+
+
+
+</div>
+
+
+
+
+
+
+<div class="container  gx-0 ">
+    <div class="row">
+
+
+        <?php
+$query_args = array(
+	'post_type' => 'post',
+);
+
+// The Query
+$the_query = new WP_Query( $query_args );
+
+// The Loop
+if ( $the_query->have_posts() ) {
+	while ( $the_query->have_posts() ) {
+		$the_query->the_post();
+?>
+        <div class="col-lg-6 col-md-12 col-sm-12 col-12  yellow-bg-div mw-100 mt-60px">
+
+            <a href="javascript:void(0);" class="album-a mobile-keep"
+                rel="<?php echo wp_get_attachment_image_src(get_field('post_enlarged_img'),'full')[0];?>">
+
+
+                <img class="w-100" src="<?php echo wp_get_attachment_image_src(get_field('post_main_img'),'full')[0];?>"
+                    alt="">
+
+            </a>
+
+
+            <h2 class="purple big mt-4">
+                <a href="<?php echo get_site_url();?>/resource-2">
+                    <?php echo get_the_title();?></a>
+            </h2>
+
+            <div class="orange">
+                <?php
+echo get_the_date('d M Y');
+?>
+
+
+            </div>
+
+            <div class="mt-3">
+
+                <?php //echo get_the_excerpt();?>
+
+                <?php echo wp_trim_words( get_the_content(), 295, '' ); ?>
+
+
+                <a href="<?php echo get_the_permalink();?>" class="orange more-link">... more</a>
+
+
+            </div>
+
+            <div class="mt-3">
+
+                <?php
+$tags_arr = get_the_tags() ;
+
+for($i=0;$i<count($tags_arr);$i++)
+{
+    ?>
+                <a href="<?php echo get_site_url();?>/tag/<?php echo $tags_arr[$i]->slug; ?>"
+                    class="blog-tag me-1">#<?php echo $tags_arr[$i]->name;?></a>
+                <?php
 }
 
+?>
+
+            </div>
+
+
+            <div class="blog-line mt-4"></div>
+
+
+
+            <div class="row mt-4">
+
+
+                <div class="col-9"><?php
+
+    pvc_post_views( get_the_ID(), $echo = true );
+
+    ?> views</div>
+                <div class="col-3 text-end">
+
+                    <?php
+    echo do_shortcode( '[posts_like_dislike id='.get_the_ID().']');
+
+    
+    ?>
+
+                    <a href="javascript:void(0);" class="d-inline-block three-dots-a ms-2"> <img class="three-dots"
+                            src="http://64.227.13.14/freeflow/wp-content/uploads/2023/01/3dots.png" alt=""></a>
+                </div>
+
+            </div>
+
+
+        </div>
+        <?php
+	}
+    ?>
+
+
+
+        <?php
+	wp_reset_postdata();
+} else {
+	// no posts found
+}
+        
+    
+    ?>
+
+
+        <!-- <div class="col-lg-6 col-md-12 col-sm-12 col-12  yellow-bg-div mw-100 mt-60px">
+
+            <a href="javascript:void(0);" class="album-a mobile-keep"
+                rel="http://64.227.13.14/freeflow/wp-content/uploads/2023/01/IMG_2481.jpg">
+                <img class="w-100" src="http://64.227.13.14/freeflow/wp-content/uploads/2023/01/blog-temp-img-1.png"
+                    alt="">
+            </a>
+
+
+            <h2 class="purple big mt-4">
+                <a href="<?php echo get_site_url();?>/resource-2">
+                    標題文章和故事</a>
+            </h2>
+
+            <div class="orange">4 Nov 2022</div>
+
+            <div class="mt-3">有關賽馬會「自主‧流」身體律動計劃的文章及資料會於這裡與大家分享。內容包括由青少女及計劃社工撰寫的文章和故事。以及，與香港浸會大學合作的研究正在進行，結果詳情將容後公佈
+                <a href="<?php echo get_site_url();?>/resource-2" class="orange more-link">... more</a>
+            </div>
+
+            <div class="mt-3"> <a href="javascript:void(0);" class="blog-tag me-1">#性</a>
+                <a href="javascript:void(0);" class="blog-tag me-1">#身體</a>
+                <a href="javascript:void(0);" class="blog-tag me-1">#自我</a>
+                <a href="javascript:void(0);" class="blog-tag">#感想</a>
+            </div>
+
+
+            <div class="blog-line mt-4"></div>
+
+
+
+            <div class="row mt-4">
+
+
+                <div class="col-9">214 views</div>
+                <div class="col-3 text-end">
+
+                    <span>10</span>
+
+                    <a href="javascript:void(0);" class="d-inline-block">
+                        <img class="heart-icon" src="http://64.227.13.14/freeflow/wp-content/uploads/2023/01/heart.png"
+                            alt=""></a>
+
+                    <a href="javascript:void(0);" class="d-inline-block three-dots-a ms-2"> <img class="three-dots"
+                            src="http://64.227.13.14/freeflow/wp-content/uploads/2023/01/3dots.png" alt=""></a>
+                </div>
+
+            </div>
+
+
+        </div> -->
+        <!-- <div class="col-lg-6 col-md-12 col-sm-12 col-12  yellow-bg-div  mw-100 mt-60px">
+
+            <a href="javascript:void(0);" class="album-a mobile-keep"
+                rel="http://64.227.13.14/freeflow/wp-content/uploads/2023/01/IMG_2505.jpg">
+
+                <img class="w-100" src="http://64.227.13.14/freeflow/wp-content/uploads/2023/01/blog-temp-img-2.png"
+                    alt="">
+            </a>
+
+
+            <h2 class="purple big mt-4">
+                <a href="<?php echo get_site_url();?>/resource-2">
+                    標題文章和故事</a>
+            </h2>
+
+            <div class="orange">4 Nov 2022</div>
+
+            <div class="mt-3">有關賽馬會「自主‧流」身體律動計劃的文章及資料會於這裡與大家分享。內容包括由青少女及計劃社工撰寫的文章和故事。以及，與香港浸會大學合作的研究正在進行，結果詳情將容後公佈
+                <a href="<?php echo get_site_url();?>/resource-2" class="orange more-link">... more</a>
+            </div>
+
+            <div class="mt-3"> <a href="javascript:void(0);" class="blog-tag me-1">#性</a>
+                <a href="javascript:void(0);" class="blog-tag me-1">#身體</a>
+                <a href="javascript:void(0);" class="blog-tag me-1">#自我</a>
+                <a href="javascript:void(0);" class="blog-tag">#感想</a>
+            </div>
+
+
+            <div class="blog-line mt-4"></div>
+
+
+
+            <div class="row mt-4">
+
+
+                <div class="col-9">214 views</div>
+                <div class="col-3 text-end">
+
+                    <span>10</span>
+
+                    <a href="javascript:void(0);" class="d-inline-block">
+                        <img class="heart-icon" src="http://64.227.13.14/freeflow/wp-content/uploads/2023/01/heart.png"
+                            alt=""></a>
+
+                    <a href="javascript:void(0);" class="d-inline-block three-dots-a ms-2"> <img class="three-dots"
+                            src="http://64.227.13.14/freeflow/wp-content/uploads/2023/01/3dots.png" alt=""></a>
+                </div>
+
+            </div>
+
+
+        </div>
+
+        <div class="col-lg-6 col-md-12 col-sm-12 col-12  yellow-bg-div  mw-100 mt-60px">
+
+
+            <a href="javascript:void(0);" class="album-a mobile-keep"
+                rel="http://64.227.13.14/freeflow/wp-content/uploads/2023/01/IMG_2481.jpg">
+
+                <img class="w-100" src="http://64.227.13.14/freeflow/wp-content/uploads/2023/01/blog-temp-img-1.png"
+                    alt="">
+            </a>
+
+
+            <h2 class="purple big mt-4"> <a href="<?php echo get_site_url();?>/resource-2">
+                    標題文章和故事</a></h2>
+
+            <div class="orange">4 Nov 2022</div>
+
+            <div class="mt-3">有關賽馬會「自主‧流」身體律動計劃的文章及資料會於這裡與大家分享。內容包括由青少女及計劃社工撰寫的文章和故事。以及，與香港浸會大學合作的研究正在進行，結果詳情將容後公佈
+                <a href="<?php echo get_site_url();?>/resource-2" class="orange more-link">... more</a>
+            </div>
+
+            <div class="mt-3"> <a href="javascript:void(0);" class="blog-tag me-1">#性</a>
+                <a href="javascript:void(0);" class="blog-tag me-1">#身體</a>
+                <a href="javascript:void(0);" class="blog-tag me-1">#自我</a>
+                <a href="javascript:void(0);" class="blog-tag">#感想</a>
+            </div>
+
+
+            <div class="blog-line mt-4"></div>
+
+
+
+            <div class="row mt-4">
+
+
+                <div class="col-9">214 views</div>
+                <div class="col-3 text-end">
+
+                    <span>10</span>
+
+                    <a href="javascript:void(0);" class="d-inline-block">
+                        <img class="heart-icon" src="http://64.227.13.14/freeflow/wp-content/uploads/2023/01/heart.png"
+                            alt=""></a>
+
+                    <a href="javascript:void(0);" class="d-inline-block three-dots-a ms-2"> <img class="three-dots"
+                            src="http://64.227.13.14/freeflow/wp-content/uploads/2023/01/3dots.png" alt=""></a>
+                </div>
+
+            </div>
+
+
+        </div>
+
+        <div class="col-lg-6 col-md-12 col-sm-12 col-12  yellow-bg-div  mw-100 mt-60px">
+
+            <a href="javascript:void(0);" class="album-a mobile-keep"
+                rel="http://64.227.13.14/freeflow/wp-content/uploads/2023/01/IMG_2505.jpg">
+
+                <img class="w-100" src="http://64.227.13.14/freeflow/wp-content/uploads/2023/01/blog-temp-img-2.png"
+                    alt="">
+            </a>
+
+
+            <h2 class="purple big mt-4"> <a href="<?php echo get_site_url();?>/resource-2">
+                    標題文章和故事</a></h2>
+
+            <div class="orange">4 Nov 2022</div>
+
+            <div class="mt-3">有關賽馬會「自主‧流」身體律動計劃的文章及資料會於這裡與大家分享。內容包括由青少女及計劃社工撰寫的文章和故事。以及，與香港浸會大學合作的研究正在進行，結果詳情將容後公佈
+                <a href="<?php echo get_site_url();?>/resource-2" class="orange more-link">... more</a>
+            </div>
+
+            <div class="mt-3"> <a href="javascript:void(0);" class="blog-tag me-1">#性</a>
+                <a href="javascript:void(0);" class="blog-tag me-1">#身體</a>
+                <a href="javascript:void(0);" class="blog-tag me-1">#自我</a>
+                <a href="javascript:void(0);" class="blog-tag">#感想</a>
+            </div>
+
+
+            <div class="blog-line mt-4"></div>
+
+
+
+            <div class="row mt-4">
+
+
+                <div class="col-9">214 views</div>
+                <div class="col-3 text-end">
+
+                    <span>10</span>
+
+                    <a href="javascript:void(0);" class="d-inline-block">
+                        <img class="heart-icon" src="http://64.227.13.14/freeflow/wp-content/uploads/2023/01/heart.png"
+                            alt=""></a>
+
+                    <a href="javascript:void(0);" class="d-inline-block three-dots-a ms-2"> <img class="three-dots"
+                            src="http://64.227.13.14/freeflow/wp-content/uploads/2023/01/3dots.png" alt=""></a>
+                </div>
+
+            </div>
+
+
+        </div>
+        <div class="col-lg-6 col-md-12 col-sm-12 col-12  yellow-bg-div  mw-100 mt-60px">
+
+
+            <a href="javascript:void(0);" class="album-a mobile-keep"
+                rel="http://64.227.13.14/freeflow/wp-content/uploads/2023/01/IMG_2481.jpg">
+
+
+                <img class="w-100" src="http://64.227.13.14/freeflow/wp-content/uploads/2023/01/blog-temp-img-1.png"
+                    alt="">
+            </a>
+
+
+            <h2 class="purple big mt-4"> <a href="<?php echo get_site_url();?>/resource-2">
+                    標題文章和故事</a></h2>
+
+            <div class="orange">4 Nov 2022</div>
+
+            <div class="mt-3">有關賽馬會「自主‧流」身體律動計劃的文章及資料會於這裡與大家分享。內容包括由青少女及計劃社工撰寫的文章和故事。以及，與香港浸會大學合作的研究正在進行，結果詳情將容後公佈
+                <a href="<?php echo get_site_url();?>/resource-2" class="orange more-link">... more</a>
+            </div>
+
+            <div class="mt-3"> <a href="javascript:void(0);" class="blog-tag me-1">#性</a>
+                <a href="javascript:void(0);" class="blog-tag me-1">#身體</a>
+                <a href="javascript:void(0);" class="blog-tag me-1">#自我</a>
+                <a href="javascript:void(0);" class="blog-tag">#感想</a>
+            </div>
+
+
+            <div class="blog-line mt-4"></div>
+
+
+
+            <div class="row mt-4">
+
+
+                <div class="col-9">214 views</div>
+                <div class="col-3 text-end">
+
+                    <span>10</span>
+
+                    <a href="javascript:void(0);" class="d-inline-block">
+                        <img class="heart-icon" src="http://64.227.13.14/freeflow/wp-content/uploads/2023/01/heart.png"
+                            alt=""></a>
+
+                    <a href="javascript:void(0);" class="d-inline-block three-dots-a ms-2"> <img class="three-dots"
+                            src="http://64.227.13.14/freeflow/wp-content/uploads/2023/01/3dots.png" alt=""></a>
+                </div>
+
+            </div>
+
+
+        </div>
+        <div class="col-lg-6 col-md-12 col-sm-12 col-12  yellow-bg-div  mw-100 mt-60px">
+
+            <a href="javascript:void(0);" class="album-a mobile-keep"
+                rel="http://64.227.13.14/freeflow/wp-content/uploads/2023/01/IMG_2505.jpg">
+
+                <img class="w-100" src="http://64.227.13.14/freeflow/wp-content/uploads/2023/01/blog-temp-img-2.png"
+                    alt="">
+            </a>
+
+            <h2 class="purple big mt-4">
+                <a href="<?php echo get_site_url();?>/resource-2">
+                    標題文章和故事</a>
+            </h2>
+
+            <div class="orange">4 Nov 2022</div>
+
+            <div class="mt-3">有關賽馬會「自主‧流」身體律動計劃的文章及資料會於這裡與大家分享。內容包括由青少女及計劃社工撰寫的文章和故事。以及，與香港浸會大學合作的研究正在進行，結果詳情將容後公佈
+                <a href="<?php echo get_site_url();?>/resource-2" class="orange more-link">... more</a>
+            </div>
+
+            <div class="mt-3"> <a href="javascript:void(0);" class="blog-tag me-1">#性</a>
+                <a href="javascript:void(0);" class="blog-tag me-1">#身體</a>
+                <a href="javascript:void(0);" class="blog-tag me-1">#自我</a>
+                <a href="javascript:void(0);" class="blog-tag">#感想</a>
+            </div>
+
+
+            <div class="blog-line mt-4"></div>
+
+
+
+            <div class="row mt-4">
+
+
+                <div class="col-9">214 views</div>
+                <div class="col-3 text-end">
+
+                    <span>10</span>
+
+                    <a href="javascript:void(0);" class="d-inline-block">
+                        <img class="heart-icon" src="http://64.227.13.14/freeflow/wp-content/uploads/2023/01/heart.png"
+                            alt=""></a>
+
+                    <a href="javascript:void(0);" class="d-inline-block three-dots-a ms-2"> <img class="three-dots"
+                            src="http://64.227.13.14/freeflow/wp-content/uploads/2023/01/3dots.png" alt=""></a>
+                </div>
+
+            </div>
+
+
+        </div> -->
+
+
+    </div>
+</div>
+
+
+<!-- 
+<div class="container gx-0 ">
+    <div class="row">
+
+
+    </div>
+</div>
+ -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- <img src="<?php echo get_template_directory_uri(); ?>/assets/images/curve.gif" alt=""> -->
+<?php
 get_footer();
