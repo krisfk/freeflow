6071d4eb5eea3a3c7f5f8d84d50f238af064113c
kaiyu li <krisfk@gmail.com>
2023-01-29 03:13:03 +0800

    .

--- wp-content/themes/freeflow/page-resource-2.php
+++ wp-content/themes/freeflow/page-resource-2.php
@@ -27,12 +27,14 @@ get_header(); ?>
 <a href="<?php echo get_site_url();?>" class="freeflow-logo-a"><img
         src="http://64.227.13.14/freeflow/wp-content/uploads/2022/12/freeflow-logo.png" alt=""></a>
 
-<div class="banner-div" style="background:url(http://64.227.13.14/freeflow/wp-content/uploads/2023/01/resource-banner-scaled.jpg);background-size: cover;
-    background-position: center center;
-    display: block;
-    width: 100%;
-    height: 860px;">
 
+<div class="banner-div" style="">
+
+    <img class="banner-img" src="http://64.227.13.14/freeflow/wp-content/uploads/2023/01/resource-banner-scaled.jpg"
+        alt="">
+
+    <img class="banner-img mobile" src="http://64.227.13.14/freeflow/wp-content/uploads/2023/01/Asset-1@2x-50-4.jpg"
+        alt="">
 
 
 
@@ -40,6 +42,7 @@ get_header(); ?>
 
 
 </div>
+
 
 
 
