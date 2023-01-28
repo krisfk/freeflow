65ec97ba74d68aea4f60bff43407aa39502a8452
kaiyu li <krisfk@gmail.com>
2023-01-29 03:14:49 +0800

    .

--- wp-content/themes/freeflow/page-resource-2.php
+++ wp-content/themes/freeflow/page-resource-2.php
@@ -71,7 +71,8 @@ get_header(); ?>
     <div class="row">
         <div class="col-12">
 
-            <img class="w-100" src="http://64.227.13.14/freeflow/wp-content/uploads/2023/01/blog-temp-img-1.png" alt="">
+            <img class="resource-detail-img"
+                src="http://64.227.13.14/freeflow/wp-content/uploads/2023/01/blog-temp-img-1.png" alt="">
 
 
 
