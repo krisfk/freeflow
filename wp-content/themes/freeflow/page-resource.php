243cf85fc8b903c380d519beb3ddf7d4b50855a0
kaiyu li <krisfk@gmail.com>
2023-01-29 04:29:59 +0800

    .

--- wp-content/themes/freeflow/page-resource.php
+++ wp-content/themes/freeflow/page-resource.php
@@ -97,9 +97,10 @@ get_header(); ?>
   $tags = get_tags(array(
     'hide_empty' => false
   ));
-//   print_r($tags);
+  print_r($tags);
 //   echo '<ul>';
 //   foreach ($tags as $tag) {
+
     for($i=0;$i<count($tags);$i++)
 {
     ?>
