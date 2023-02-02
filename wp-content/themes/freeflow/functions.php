a409ae72d5b342796f943a0486f9081f9b895532
kaiyu li <krisfk@gmail.com>
2023-02-02 18:44:10 +0800

    .

--- wp-content/themes/freeflow/functions.php
+++ wp-content/themes/freeflow/functions.php
@@ -721,3 +721,12 @@ function wp_get_menu_array($current_menu='Main Menu') {
 	return $menu;
 
 }
+
+function get_id_by_slug($page_slug) {
+    $page = get_page_by_path($page_slug);
+    if ($page) {
+        return $page->ID;
+    } else {
+        return null;
+    }
+} 
--- wp-content/themes/freeflow/page-home.php
+++ wp-content/themes/freeflow/page-home.php
@@ -55,8 +55,8 @@ get_header(); ?>
             <tr>
                 <td>
 
-
-                    <?php echo get_the_title( get_page_by_path( 'general-config' ));?>
+                    <!-- get_id_by_slug -->
+                    <?php echo  get_id_by_slug( 'general-config' );?>
                     <?php if( have_rows('partnership_logos_group_1',get_the_title( get_page_by_path( 'general-config' ) )) ): ?>
                     <?php while( have_rows('partnership_logos_group_1',get_the_title( get_page_by_path( 'general-config' ) )) ): the_row();                      ?>
 
