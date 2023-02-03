cce8921583ce87f3bd3bfc5cffe2210daa202a00
kaiyu li <krisfk@gmail.com>
2023-02-04 05:38:23 +0800

    .

--- wp-content/themes/freeflow/functions.php
+++ wp-content/themes/freeflow/functions.php
@@ -733,7 +733,7 @@ function get_id_by_slug($page_slug) {
 
 function my_change_posts_order( $query ){
     if ( ! is_admin() && ( is_category() || is_tag() ) && $query->is_main_query() ) {
-        $query->set( 'order', 'DESC' );
+        $query->set( 'order', 'ASC' );
     }
 };
 add_action( 'pre_get_posts', 'my_change_posts_order'); 
