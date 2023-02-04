d37bc4a25ad2ca682e040b28b7a25bb2e1cda223
kaiyu li <krisfk@gmail.com>
2023-02-05 02:34:58 +0800

    .

--- wp-content/themes/freeflow/single.php
+++ wp-content/themes/freeflow/single.php
@@ -30,12 +30,21 @@ get_header(); ?>
 
 <div class="banner-div" style="">
 
-    <img class="banner-img" src="http://64.227.13.14/freeflow/wp-content/uploads/2023/01/resource-banner-scaled.jpg"
+
+
+
+    <!-- <img class="banner-img" src="http://64.227.13.14/freeflow/wp-content/uploads/2023/01/resource-banner-scaled.jpg"
         alt="">
 
     <img class="banner-img mobile" src="http://64.227.13.14/freeflow/wp-content/uploads/2023/01/Asset-1@2x-50-4.jpg"
-        alt="">
+        alt=""> -->
 
+
+    <img class="banner-img"
+        src="<?php echo wp_get_attachment_image_src(get_field('activity_top_banner_desktop',778),'full')[0];?>" alt="">
+
+    <img class="banner-img mobile"
+        src="<?php echo wp_get_attachment_image_src(get_field('activity_top_banner_mobile',778),'full')[0];?>" alt="">
 
 
 
