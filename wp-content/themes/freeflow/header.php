fe367c77337655b86557b7982b386b714e8cae5e
kaiyu li <krisfk@gmail.com>
2023-02-07 01:59:26 +0900

    .

--- wp-content/themes/freeflow/header.php
+++ wp-content/themes/freeflow/header.php
@@ -38,6 +38,12 @@ $general_config_id =  $current_language_code=='zh-hant' ? 323:810;
     <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/pace-js@latest/pace-theme-default.min.css"> -->
 
 
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600&display=swap"
+        rel="stylesheet">
+
     <?php wp_head(); ?>
 </head>
 
