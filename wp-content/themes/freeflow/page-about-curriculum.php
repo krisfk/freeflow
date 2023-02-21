625c2b95c351243d1c4aefe13398823db2ae7eaa
kaiyu li <krisfk@gmail.com>
2023-02-21 10:30:47 +0800

    .

--- wp-content/themes/freeflow/page-about-curriculum.php
+++ wp-content/themes/freeflow/page-about-curriculum.php
@@ -360,7 +360,7 @@ endwhile; ?>
                     <div class="row">
 
                         <div class="col-5">
-                            <div class="green-bubble"><?php echo get_field('step_2_name');?></div>
+                            <div class="green-bubble two-line"><?php echo get_field('step_2_name');?></div>
                         </div>
 
                         <div class="col-2">
